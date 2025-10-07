59a141755ee6c3fcfbc1eacffc297dff7ce9a168
vboxuser <vboxuser@WIN10>
2025-10-07 16:29:13 +0300

    Создана модель корзины + интерфейс

---
+++ PencilDelivery/Models/Cart.cs
@@ -0,0 +1,24 @@
+﻿namespace PencilDelivery.Models
+{
+    public class Cart
+    {
+        private List<Product> _products = new();
+
+        public int Count => _products.Count;
+
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public decimal TotalPrice()
+        {
+            return _products.Sum(product => product.PricePerUnit);
+        }
+
+        public void Clear()
+        {
+            _products.Clear();
+        }
+    }
+}
--- PencilDelivery/Pages/Products/Index.cshtml.cs
+++ PencilDelivery/Pages/Products/Index.cshtml.cs
@@ -18,18 +18,22 @@ namespace PencilDelivery.Pages.Products
             _context = context;
         }
 
-        public IList<Product> Product { get;set; } = default!;
-        public IList<Category> Category { get;set; } = default!;
-        public IList<Photo> Photo { get;set; } = default!;
+        public IList<Product> Product { get; set; } = default!;
+        public IList<Category> Category { get; set; } = default!;
+        public IList<Photo> Photo { get; set; } = default!;
 
         [BindProperty(SupportsGet = true)]
         public string? SearchString { get; set; }
         [BindProperty(SupportsGet = true)]
         public Category SelectedCategory { get; set; }
-        
+        [BindProperty(SupportsGet = true)]
+        public Product SelectedProduct { get; set; }
+
         public async Task<IActionResult> OnGetAsync(int? id)
         {
+
             Category = await _context.Categories.ToListAsync();
+            Category.Insert(0, new Category() { Id = 0, Name = "Все товары", IconUrl = "https://img.vkusvill.ru/pim/images/site_grp_Webp/4fe95bc148b5f9771bb8fdcc46f79f7ce255bfd2.webp?1756795240.4456" });
 
             Photo = await _context.Photos
                 .Include(p => p.Product).ToListAsync();
@@ -45,6 +49,13 @@ namespace PencilDelivery.Pages.Products
             }
 
 
+            if (id == 0)
+            {
+                Product = await _context.Products.ToListAsync();
+
+                return Page();
+            }
+
             if (id != null)
             {
                 SelectedCategory = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
@@ -54,6 +65,14 @@ namespace PencilDelivery.Pages.Products
             }
 
             return Page();
+        }
+        public async Task OnPostAsync(int? id)
+        {
+            SelectedProduct = _context.Products.FirstOrDefaultAsync(p => p.Id == id).Result;
+
+            Cart cart = new();
+
+            cart.Add(SelectedProduct);
         }
     }
 }
